62fbde6c261e656b55a5d37036dce6c6d42bb477
yulia <julie_gal@mail.ru>
2017-03-14 19:18:07 +0300

    project

--- hello.py
+++ hello.py
@@ -1,12 +1,14 @@
 import re
 
 def app(environ, start_response):
-    status = "200 OK"
-    headers = [("Content-Type", "text/plain")]
     qs = environ['QUERY_STRING']
     pattern = r"[0-9a-z]+=[0-9a-z]+"
     result = []
     if re.search(pattern, qs):
         result = re.findall(pattern, qs)
-    start_response(status, headers)
-    return "\r\n".join(result).encode()
+    result = "\r\n".join(result)
+    start_response("200 OK", [
+        ("Content-Type", "text/plain")])
+    return [i.encode() for i in result]
+   
+    
