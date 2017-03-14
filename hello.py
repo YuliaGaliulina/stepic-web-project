98a24da8c04df96ed1c62182a35d48fa667fc012
yulia <julie_gal@mail.ru>
2017-03-14 20:15:08 +0300

    regex changed --> \w

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import re
 
 def app(environ, start_response):
     qs = environ['QUERY_STRING']
-    pattern = r"[0-9A-Za-z]+=[0-9A-za-z]+|[0-9A-Za-z]+="
+    pattern = r"\w+=\w+|\w+="
     result = []
     if re.search(pattern, qs):
         result = re.findall(pattern, qs)
